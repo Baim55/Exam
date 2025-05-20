317dbb60b042cce663a224aa9c76c6f747c2231f
mustafavahabovv <mustafajv-af107@code.edu.az>
2025-05-20 18:48:57 +0400

    bilet

---
+++ Bilet 14/backend/src/routes/productRouter.js
@@ -0,0 +1,10 @@
+import express from "express"
+import { createProduct, deleteProduct, getProducts } from "../controllers/productController.js"
+
+const productRouter = express.Router()
+
+productRouter.get("/", getProducts)
+productRouter.delete("/:id", deleteProduct)
+productRouter.post("/", createProduct)
+
+export default productRouter
